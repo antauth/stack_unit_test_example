0aa10d1bbb128d404c3a0bfacf69d1a9715f3d4f
Antoinette Smith <antoinette@primeacademy.io>
2015-11-15 19:43:43 -0600

    Initial setup with Stack function stubs and single unit test

---
+++ app.js
@@ -0,0 +1,57 @@
+/**
+ * Stack implementation using JavaScript arrays.
+ * @author antoinette@primeacademy.io
+ */
+
+ function Stack() {
+ 	var elements = [];
+
+ 	/**
+ 	 * Adds one or more elements to top of stack.
+ 	 *
+ 	 * @param {*} an array of elements
+ 	 */
+ 	this.push = function(element) {
+ 		return null;
+ 	}
+ 	/**
+ 	 * Removes top item from stack.
+ 	 * Returns removed element.
+ 	 *
+ 	 * @return removed element
+ 	 */
+ 	this.pop = function() {
+ 		return null;
+ 	}
+ 	/**
+ 	 * Returns top element of stack.
+ 	 * Does NOT remove element.
+ 	 *
+ 	 * @return top element of stack
+ 	 */
+ 	this.peek = function() {
+ 		return null;
+ 	}
+ 	/**
+ 	 * Determines if stack has elements.
+ 	 *
+ 	 * @return true if no elements, false if 1 or more elements
+ 	 */
+ 	this.isEmpty = function() {
+ 		return null;
+ 	}
+ 	/**
+ 	 * Removes all elements of stack.
+ 	 */
+ 	this.clear = function() {
+ 		return null;
+ 	}
+ 	/**
+ 	 * Determines number of elements in stack.
+ 	 *
+ 	 * @return number of elements in stack
+ 	 */
+ 	this.size = function() {
+ 		return null;
+ 	}
+ }
---
+++ tests.js
@@ -0,0 +1,5 @@
+QUnit.module("basic");
+QUnit.test("initialize object", function(assert) {
+	assert.ok(new Stack(), "can create new Stack")
+});
+
