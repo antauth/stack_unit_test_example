987ea79f295f54ce0fcfcf4559301b900a3fb174
Antoinette Smith <antoinette@primeacademy.io>
2015-11-15 19:46:42 -0600

    Added tests for push function

--- tests.js
+++ tests.js
@@ -3,3 +3,21 @@ QUnit.test("initialize object", function(assert) {
 	assert.ok(new Stack(), "can create new Stack")
 });
 
+QUnit.module("push", {
+	beforeEach: function() {
+		this.stack = new Stack();
+		console.log(this.stack);
+	}
+});
+
+QUnit.test("Can add one item to stack", function(assert) {
+	this.stack.push("first");
+	assert.equal(this.stack.size(), 1, "stack has one element");
+});
+
+QUnit.test("Can add three items to stack", function(assert) {
+	this.stack.push("first");
+	this.stack.push("second");
+	this.stack.push("third");
+	assert.equal(this.stack.size(), 3, "stack has three elements");
+});
